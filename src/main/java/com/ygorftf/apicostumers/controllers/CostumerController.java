db708926e3b250d3104abfa0890e5a79ac330753
ygorftf <ygorftf@gmail.com>
2024-02-03 21:02:50 -0300

    findCostumerById

--- src/main/java/com/ygorftf/apicostumers/controllers/CostumerController.java
+++ src/main/java/com/ygorftf/apicostumers/controllers/CostumerController.java
@@ -1,8 +1,12 @@
 package com.ygorftf.apicostumers.controllers;
 
+import com.ygorftf.apicostumers.dto.CostumerDTO;
 import com.ygorftf.apicostumers.services.CostumerService;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +16,9 @@ public class CostumerController {
     @Autowired
     private CostumerService service;
 
-
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CostumerDTO> findCostumerById(@PathVariable Long id){
+        CostumerDTO dto = service.findCostumerById(id);
+        return ResponseEntity.ok(dto);
+    }
 }
--- src/main/java/com/ygorftf/apicostumers/services/CostumerService.java
+++ src/main/java/com/ygorftf/apicostumers/services/CostumerService.java
@@ -1,13 +1,22 @@
 package com.ygorftf.apicostumers.services;
 
+import com.ygorftf.apicostumers.dto.CostumerDTO;
+import com.ygorftf.apicostumers.entities.Costumer;
 import com.ygorftf.apicostumers.repositories.CostumerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CostumerService {
 
     @Autowired
     private CostumerRepository repository;
+
+    public CostumerDTO findCostumerById(Long id) {
+        Optional<Costumer> costumer = repository.findById(id);
+        return new CostumerDTO(costumer);
+    }
 
 }
